a848016389b745bffc9ad1bad5683d8f0930a0ca
ZeyuGuo <tonyguo918@gmail.com>
2020-08-16 11:39:37 +0800

    modify homepage style

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <van-notice-bar mode="closeable"
-                    left-icon="volume-o"
+                    left-icon="like-o"
                     text="答案很长，我准备用一生的时间来回答，你准备好听了吗？"
     />
-
+    <Encounter></Encounter>
     <div class="img-center-pos">
-      <img width="90%" src="@/assets/img/home/home_background.png" />
+      <img src="@/assets/img/home/home_background.png" />
     </div>
   </div>
 
 </template>
 
 <script>
+  import Encounter from "./childComponents/Encounter";
+
   import Vue from 'vue';
   import { NoticeBar,Button } from 'vant';
 
@@ -22,7 +24,7 @@
   export default {
     name: 'Home',
     components: {
-
+      Encounter
     },
     data() {
       return {
@@ -43,6 +45,11 @@
     justify-content: center;
     position: absolute;
     bottom: 50px;
+  }
+
+  .img-center-pos img {
+    width: 90%;
+    height: 90%;
   }
 
 </style>
--- src/views/home/childComponents/Encounter.vue
+++ src/views/home/childComponents/Encounter.vue
@@ -1,15 +1,98 @@
 <template>
-    <div class="encounter">
-
+  <div class="encounter">
+    <div class="outer">
+      <div class="img-inner">
+        <van-image
+          round
+          fit="cover"
+          width="5rem"
+          height="5rem"
+          src="http://m.xiaoxiaozeyu.online/images/avatar/avatar_1.jpg"
+        />
+      </div>
+      <div class="inner">
+        <div>
+          <p>我们相恋了{{loveTime}}天</p>
+        </div>
+        <div>
+          <van-icon name="http://m.xiaoxiaozeyu.online/images/icon/line.svg"/>
+          <van-icon name="http://m.xiaoxiaozeyu.online/images/icon/line.svg"/>
+          <van-icon name="http://m.xiaoxiaozeyu.online/images/icon/line.svg"/>
+          <van-icon class="like-icon" name="like" color="#FF8799" />
+          <van-icon name="http://m.xiaoxiaozeyu.online/images/icon/line.svg"/>
+          <van-icon name="http://m.xiaoxiaozeyu.online/images/icon/line.svg"/>
+          <van-icon name="http://m.xiaoxiaozeyu.online/images/icon/line.svg"/>
+        </div>
+      </div>
+      <div class="img-inner">
+        <van-image
+          round
+          fit="cover"
+          width="5rem"
+          height="5rem"
+          src="http://m.xiaoxiaozeyu.online/images/avatar/avatar_2.jpg"
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Encounter"
+  import Vue from 'vue';
+  import { Image as VanImage } from 'vant';
+  import { Icon } from 'vant';
+
+  Vue.use(VanImage);
+  Vue.use(Icon);
+
+  export default {
+    name: "Encounter",
+    data() {
+      return {
+        encounterTime: new Date(2020,8,15)
+      }
+    },
+    computed: {
+      loveTime() {
+        let today = new Date();
+        let currentDay =new Date(today.getFullYear(),today.getMonth()+1, today.getDate())
+        return (currentDay.getTime()-this.encounterTime.getTime())/(1000 * 60 * 60 * 24);
+      }
     }
+  }
 </script>
 
 <style scoped>
+  .encounter{
 
+  }
+
+  .outer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 5px 15px;
+    margin: 65px 0 0 0;
+  }
+
+  .img-inner {
+    flex: 1;
+  }
+
+  .inner {
+    display: flex;
+    flex-direction: column;
+    flex: 2;
+  }
+
+  .inner div p {
+    margin: 10px 0;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .like-icon {
+    margin: 0 8px;
+  }
 </style>
